8724c36e1fcf41f67f9d568068ec82531dcd077d
Monomaze <brian@wester-mail.de>
2024-09-30 19:24:21 +0200

    import FeaturedProjectsComponets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { FeaturedProjectsComponent } from './components/featured-projects/featured-projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, AboutMeComponent, SkillsComponent, FeaturedProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
