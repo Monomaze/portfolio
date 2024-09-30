7fd2f5863420037d1a1fe2e28dcaaab2a476f73b
Monomaze <brian@wester-mail.de>
2024-09-30 21:34:07 +0200

    add content and design to component

--- src/app/components/featured-projects/project-list/project-list.component.ts
+++ src/app/components/featured-projects/project-list/project-list.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './project-list.component.scss'
 })
 export class ProjectListComponent {
-
+  projectEntry = [
+    {
+      title: "Join",
+      tech: "Javascript | HTML | CSS | Firebase",
+      imgPath: "/assets/images/pictures/join.png"
+    },
+    {
+      title: "Mana Trails",
+      tech: "JavaScript | HTML | CSS",
+      imgPath: "/assets/images/pictures/manatrails.png"
+    },
+    {
+      title: "Pokedex",
+      tech: "JavaScript | HTML | CSS | API",
+      imgPath: "/assets/images/pictures/pokedex.png"
+    },
+    {
+      title: "My Portfolio",
+      tech: "Angular | TypeScript | HTML | SCSS",
+      imgPath: "/assets/images/pictures/portfolio.png"
+    }
+  ]
 }
