40ea2a332aab8f78e9b1f2a31902e43e3eef7d06
Monomaze <brian@wester-mail.de>
2024-09-30 21:34:54 +0200

    import ProjectListComponent and add component to html

--- src/app/components/featured-projects/featured-projects.component.ts
+++ src/app/components/featured-projects/featured-projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @Component({
   selector: 'app-featured-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectListComponent],
   templateUrl: './featured-projects.component.html',
   styleUrl: './featured-projects.component.scss'
 })
