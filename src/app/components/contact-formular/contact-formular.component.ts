16e6038cfce7bba082e0a95cfeca90aa90b08bdc
Monomaze <brian@wester-mail.de>
2024-12-20 10:01:01 +0100

    add contact formular functionality and complete validation

--- src/app/components/contact-formular/contact-formular.component.ts
+++ src/app/components/contact-formular/contact-formular.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-formular',
@@ -9,6 +10,8 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './contact-formular.component.scss'
 })
 export class ContactFormularComponent {
+  editable = false;
+  http = inject(HttpClient);
 
   contactData = {
     name: "",
@@ -16,10 +19,45 @@ export class ContactFormularComponent {
     message: ""
   }
 
-  onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
-    }
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
   }
 
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    } 
+  }
+
+  toggleEditable() {
+    let error: any = document.getElementById('privacy-error');
+    this.editable = !this.editable;
+    if (this.editable == false) {
+      error.style.display = 'unset';
+    } else {
+      error.style.display = 'none';
+    }
+  }
 }
